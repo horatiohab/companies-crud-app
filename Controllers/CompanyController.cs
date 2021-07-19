3e72e1f88bafad6c9569fdadf4c5b7daa438478b
horatiohab <horatiohab@gmail.com>
2021-07-19 12:57:16 +0100

    Add update and create new entry functionality

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -43,6 +43,26 @@ namespace CompaniesApp.Controllers
             return View(company);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CompanyId,Name,Email,Logo,Website")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != company.CompanyId)
+                {
+                    _context.Add(company);
+                }
+                else
+                {
+                    _context.Update(company);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
        /* #region API CALLS
 
         [HttpGet]
